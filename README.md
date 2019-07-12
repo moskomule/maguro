623f2cb6c2ced79b9ed4736a8245ee40bfff5515
moskomule <hataya@keio.jp>
2019-07-12 12:36:27 +0900

    wakaran

--- README.md
+++ README.md
@@ -4,18 +4,16 @@
 
 ## Usage
 
-Prepare a task list file at first (suppose the file name is `tuna` ).
+Suppose you want to run the following command `NUM_REPEAT` times.
 
 ```
 python train.py --model resnet20
-python train.py --model resnet56
-python train.py --model vgg19
 ```
 
 Then, if you run these tasks three times each,
 
 ```
-maguro tuna -r NUM_REPEAT -n NUM_GPU_PER_TRIAL
+maguro -r NUM_REPEAT -n NUM_GPU_PER_TRIAL python train.py --model resnet20
 ```
 
 You can use `--dryrun` to check which commands will be executed.
--- maguro/maguro.py
+++ maguro/maguro.py
@@ -1,22 +1,24 @@
+import asyncio
+import os as python_os
 import pathlib
 import subprocess
-import argparse
-import asyncio
-import logging
-import os as python_os
 from functools import partial
-from datetime import datetime
+
 from .utils import NOW, logger, COLOR, TicketSeller, parse_args, devices
 
 
-async def run(command, env, output):
+async def run(command,
+              env,
+              output):
     # make subprocess.run async
     loop = asyncio.get_event_loop()
     return await loop.run_in_executor(None, partial(subprocess.run, command, env=env,
                                                     stdout=output, stderr=subprocess.STDOUT))
 
 
-async def distribute_trial(command, trial_id, args):
+async def distribute_trial(command,
+                           trial_id,
+                           args):
     ticket = TicketSeller()
     while ticket.is_soldout(args.num_gpu_per_trial):
         await asyncio.sleep(10)
@@ -30,14 +32,15 @@ async def distribute_trial(command, trial_id, args):
         logger.info(f"start: {cvd} {command}")
         current_env["CUDA_VISIBLE_DEVICES"] = devices(gpu_ids)
         with (pathlib.Path(args.log_dir) / f"{NOW}-{trial_id:0>4}.log").open('w') as log_file:
-            log_file.write(f"maguro {NOW}\n{command}\n{'-'*10}\n\n")
+            log_file.write(f"maguro {NOW}\n{command}\n{'-' * 10}\n\n")
             log_file.flush()
             await run(command.split(), env=current_env, output=log_file)
     ticket.sell(gpu_ids)
     logger.info(COLOR.colored_str(f"finish: {cvd} {command}", COLOR.GREEN))
 
 
-async def _main(commands, args):
+async def _main(commands,
+                args):
     await asyncio.gather(*[distribute_trial(command, trial_id, args)
                            for trial_id, command in enumerate(commands)])
 
